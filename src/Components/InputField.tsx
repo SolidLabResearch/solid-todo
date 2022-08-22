6a8cdb4c9f044ed611dcea1579fbb83cbc39dfcd
vidyashreetarikereugent <vidyashree.tarikere@ugent.be>
2022-08-22 21:02:45 +0200

    to write to solid

--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,16 +1,54 @@
-import React, { useRef } from 'react'
+import  { FormEvent, useRef, useState } from 'react'
+import { useSession } from "@inrupt/solid-ui-react";
+import {
+  addDatetime,
+  addStringNoLocale,
+  addUrl,
+  createThing,
+  getSourceUrl,
+  saveSolidDatasetAt,
+  setThing,
+} from "@inrupt/solid-client";
 import './style.css'
 
-interface Props{
-    todo: string;
-    setTodo: React.Dispatch<React.SetStateAction<string>>;
-    handleAdd: (e:React.FormEvent) => void;
-}
+const InputField = ({ todos, setTodos }) => {
 
-const InputField: React.FC<Props> = ({todo, setTodo, handleAdd} : Props) => {
-
+  const { session } = useSession();
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const [todo, setTodo] = useState<string>("");
+
+const TEXT_PREDICATE = "http://schema.org/text";
+const CREATED_PREDICATE = "http://schema.org/dateCreated";
+const TODO_VALUE = "http://schema.org/TextDigitalDocument";
+const TODO_PROPERTY = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+
+
+  const addTodo = async (text: string) => {
+    const indexUrl = getSourceUrl(todos);
+    const todoWithText = addStringNoLocale(createThing(), TEXT_PREDICATE, text);
+    console.log(createThing);
+    const todoWithDate = addDatetime(
+      todoWithText,
+      CREATED_PREDICATE,
+      new Date()
+    );
+    const todoWithType = addUrl(todoWithDate, TODO_PROPERTY, TODO_VALUE);
+    const updatedTodoList = setThing(todos, todoWithType);
+    const updatedDataset = await saveSolidDatasetAt(indexUrl, updatedTodoList, {
+      fetch: session.fetch as any,
+    });
+    setTodos(updatedDataset);
+  };
+
+  const handleAdd = async (event) => {
+    event.preventDefault();
+    addTodo(todo);
+    setTodo("");
+  };
+
+  // const handleChange
+  
   return (
     <form className='input' id= 'input' onSubmit= {(e) =>{handleAdd(e)
     inputRef.current?.blur();}}>
