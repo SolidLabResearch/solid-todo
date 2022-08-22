2293232a2bf282406c981ec8bb50007a0f5e3702
vidyashreetarikereugent <vidyashree.tarikere@ugent.be>
2022-08-22 21:03:36 +0200

    moved login to login.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,23 @@
+import App from "./App";
+import "./index.css"
+import { SessionProvider } from "@inrupt/solid-ui-react";
+import { createRoot } from "react-dom/client"
+
+// ReactDOM.render(
+//   <SessionProvider>
+//     <App />
+//   </SessionProvider>,
+//   document.getElementById("root")
+// );
+
+const root = createRoot(document.getElementById("root")!)
+root.render(<SessionProvider> <App /> </SessionProvider>)
+
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-import "./index.css"
-import App from "./App"
-import { createRoot } from "react-dom/client"
+// import "./index.css"
+// import App from "./App"
+// import { createRoot } from "react-dom/client"
 //import InputField from './Components/InputField';
 
 // import {
@@ -13,59 +28,59 @@ import { createRoot } from "react-dom/client"
 //   setStringNoLocale,
 //   saveSolidDatasetAt
 // } from "@inrupt/solid-client";
-import { Session } from "@inrupt/solid-client-authn-browser"
+//import { Session } from "@inrupt/solid-client-authn-browser"
 //import { VCARD } from "@inrupt/vocab-common-rdf";
 
 // for testing purposes
-import { findTodoEntries } from "./query"
-import { ITodo } from "./model"
+//import { findTodoEntries } from "./query"
+//import { ITodo } from "./model"
 
 //const NOT_ENTERED_WEBID ="...not logged in yet - but enter any WebID to read from its profile...";
 
-const session = new Session()
-//console.log(session);
-const buttonLogin = document.getElementById("btnLogin")
+// const session = new Session()
+// //console.log(session);
+// const buttonLogin = document.getElementById("btnLogin")
 
 //const writeForm = document.getElementById("input") as HTMLInputElement;
 //const readForm = document.getElementById("readForm");
 
 // 1a. Start Login Process. Call session.login() function.
-async function login() {
-  if (!session.info.isLoggedIn) {
-    const identityProviderSelection: HTMLSelectElement = document.getElementById("identityProviderSelection") as HTMLSelectElement
-    console.log(`Login with ${identityProviderSelection.value}`)
-    await session.login({
-      oidcIssuer: identityProviderSelection.value,
-      clientName: "Inrupt tutorial client app",
-      redirectUrl: window.location.href
-    })
-  }
-}
+// async function login() {
+//   if (!session.info.isLoggedIn) {
+//     const identityProviderSelection: HTMLSelectElement = document.getElementById("identityProviderSelection") as HTMLSelectElement
+//     console.log(`Login with ${identityProviderSelection.value}`)
+//     await session.login({
+//       oidcIssuer: identityProviderSelection.value,
+//       clientName: "Inrupt tutorial client app",
+//       redirectUrl: window.location.href
+//     })
+//   }
+// }
 
-// 1b. Login Redirect. Call session.handleIncomingRedirect() function.
-// When redirected after login, finish the process by retrieving session information.
-async function handleRedirectAfterLogin() {
-  await session.handleIncomingRedirect(window.location.href)
-  if (session.info.isLoggedIn) {
-    // Update the page with the status.
-    document.getElementById("labelStatus")!.innerHTML = `Your session is logged in with the WebID [<a target="_blank" href="${session.info.webId}">${session.info.webId}</a>].`
-    document.getElementById("labelStatus")!.setAttribute("role", "alert")
+// // 1b. Login Redirect. Call session.handleIncomingRedirect() function.
+// // When redirected after login, finish the process by retrieving session information.
+// async function handleRedirectAfterLogin() {
+//   await session.handleIncomingRedirect(window.location.href)
+//   if (session.info.isLoggedIn) {
+//     // Update the page with the status.
+//     document.getElementById("labelStatus")!.innerHTML = `Your session is logged in with the WebID [<a target="_blank" href="${session.info.webId}">${session.info.webId}</a>].`
+//     document.getElementById("labelStatus")!.setAttribute("role", "alert")
 
     //document.getElementById("webID")! as HTMLElement = session.info.webId;
 
     // for testing purposes only
-    findTodoEntries(session)
-      .then((todos: ITodo[]) => {
-        console.log(JSON.stringify(todos))
-      })
-      .catch((reason: any) => console.log(reason))
+    // findTodoEntries(session)
+    //   .then((todos: ITodo[]) => {
+    //     console.log(JSON.stringify(todos))
+    //   })
+    //   .catch((reason: any) => console.log(reason))
 
-    const root = createRoot(document.getElementById("root")!)
-    root.render(<App />)
-  }
-}
+    // const root = createRoot(document.getElementById("root")!)
+    // root.render(<App />)
+//   }
+// }
 
-handleRedirectAfterLogin()
+// handleRedirectAfterLogin()
 
 //Write to profile
 // async function writeProfile() {
@@ -90,7 +105,7 @@ handleRedirectAfterLogin()
 //   }
 
 // }
-buttonLogin!.addEventListener("click", login)
+//buttonLogin!.addEventListener("click", login)
 
 // writeForm.addEventListener("submit", (event) => {
 //    event.preventDefault();
