af4b591aeedda86f428cebbecde3487c99197f24
vidyashreetarikereugent <vidyashree.tarikere@ugent.be>
2022-09-02 15:12:50 +0200

    Sparql query to write to pods

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,38 +1,52 @@
 import { useRef, useState } from 'react'
-import { useSession } from '@inrupt/solid-ui-react'
-import { addDatetime, addStringNoLocale, addUrl, createThing, getSourceUrl, saveSolidDatasetAt, setThing } from '@inrupt/solid-client'
+//import { useSession } from '@inrupt/solid-ui-react'
+import { fetch } from "@inrupt/solid-client-authn-browser";
+//import { addDatetime, addStringNoLocale, addUrl, createThing, getSourceUrl, saveSolidDatasetAt, setThing } from '@inrupt/solid-client'
 
 interface IInputFieldProps {
   todos: any
   setTodos: any
 }
 
-const InputField: React.FC<IInputFieldProps> = ({ todos, setTodos }: IInputFieldProps): JSX.Element => {
-  const { session } = useSession()
+const InputField = ({ todos, setTodos, file}: any) => {
+  //const { session } = useSession()
   const inputRef = useRef<HTMLInputElement>(null)
 
   const [todo, setTodo] = useState<string>('')
 
-  const TEXT_PREDICATE = 'http://schema.org/text'
-  const CREATED_PREDICATE = 'http://schema.org/dateCreated'
-  const TODO_VALUE = 'http://schema.org/TextDigitalDocument'
-  const TODO_PROPERTY = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+  // const TEXT_PREDICATE = 'http://schema.org/text'
+  // const CREATED_PREDICATE = 'http://schema.org/dateCreated'
+  // const TODO_VALUE = 'http://schema.org/TextDigitalDocument'
+  // const TODO_PROPERTY = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
 
-  function addTodo(text: string): void {
-    const indexUrl = getSourceUrl(todos)
-    const todoWithText = addStringNoLocale(createThing(), TEXT_PREDICATE, text)
-    console.log(createThing)
-    const todoWithDate = addDatetime(
-      todoWithText,
-      CREATED_PREDICATE,
-      new Date()
-    )
-    const todoWithType = addUrl(todoWithDate, TODO_PROPERTY, TODO_VALUE)
-    const updatedTodoList = setThing(todos, todoWithType)
+  const addTodo = async (text: string) => {
 
-    saveSolidDatasetAt(indexUrl, updatedTodoList, { fetch: session.fetch as any })
-      .then((updatedDataset) => setTodos(updatedDataset))
-      .catch((reason: any) => console.log(reason))
+    const query= `INSERT DATA {<TodoList> <TodoItem> <${text}>.}`;
+
+    const response = await fetch(file, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/sparql-update'},
+      body: query,
+    });
+    if (300<response.status&&response.status<600) {
+      console.log(` Error code is ${response.status} 
+      Your friend may not have used our app yet!`);
+    }
+
+    // const indexUrl = getSourceUrl(todos)
+    // const todoWithText = addStringNoLocale(createThing(), TEXT_PREDICATE, text)
+    // console.log(createThing)
+    // const todoWithDate = addDatetime(
+    //   todoWithText,
+    //   CREATED_PREDICATE,
+    //   new Date()
+    // )
+    // const todoWithType = addUrl(todoWithDate, TODO_PROPERTY, TODO_VALUE)
+    // const updatedTodoList = setThing(todos, todoWithType)
+
+    // saveSolidDatasetAt(indexUrl, updatedTodoList, { fetch: session.fetch as any })
+    //   .then((updatedDataset) => setTodos(updatedDataset))
+    //   .catch((reason: any) => console.log(reason))
   }
 
   function handleAdd(event: React.FormEvent<HTMLFormElement>): void {
