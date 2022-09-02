6c8db4a6e97df864e45405688128ba45d3f48c27
vidyashreetarikereugent <vidyashree.tarikere@ugent.be>
2022-09-02 15:13:18 +0200

    creating a folder with querying

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from 'react'
 import { LoginButton, LogoutButton, Text, useSession, CombinedDataProvider } from '@inrupt/solid-ui-react'
-import { getSolidDataset, getUrlAll, getThing } from '@inrupt/solid-client'
-import { ITodo } from '../logic/model'
+// import { getSolidDataset, getUrlAll, getThing } from '@inrupt/solid-client'
+// import { ITodo } from '../logic/model'
 import { findOidcIssuer } from '../logic/query'
-import { getOrCreateTodoList, getTodoListViaQuery } from '../logic/utils'
+//import { getOrCreateTodoList, getTodoListViaQuery } from '../logic/utils'
 import InputField from './InputField'
 import TodoList from './TodoList'
+import { QueryEngine } from '@comunica/query-sparql'
 
 const authOptions = {
   clientName: 'Solid Todo App'
 }
 
-const STORAGE_PREDICATE = 'http://www.w3.org/ns/pim/space#storage'
+//const STORAGE_PREDICATE = 'http://www.w3.org/ns/pim/space#storage'
 
 const keywordToProviderMap: Map<string, string> = new Map<string, string>([
   ['solidcommunity', 'https://solidcommunity.net/'],
@@ -22,27 +23,66 @@ const Login: React.FC = (): JSX.Element => {
   const { session } = useSession()
   const [oidcIssuer, setOidcIssuer] = useState('http://localhost:3000')
 
+  const [file, setFile] = useState("") 
+
   // const [todos, setTodos] = useState<ITodo[]>(new Array<ITodo>())
   const [todos, setTodos] = useState<any>()
 
   useEffect(() => {
     if (!session.info.isLoggedIn) return
-    getTodoListViaQuery(session)
-      .then((todos: ITodo[]) => { console.log(todos) })
-      .catch((reason: any) => console.log(reason))
-    void (async () => {
-      const profileDataset = await getSolidDataset(session.info.webId as any, {
-        fetch: session.fetch as any
-      })
-      const profileThing = getThing(profileDataset, session.info.webId as any)
-      const podsUrls = getUrlAll(profileThing as any, STORAGE_PREDICATE)
-      console.log(podsUrls)
-      const pod = podsUrls[0]
-      const containerUri = `${pod}todos/`
-      const list = await getOrCreateTodoList(containerUri, session.fetch)
-      setTodos(list)
-      // console.log(list)
-    })()
+
+    (async  () => {
+    
+      const myEngine = new QueryEngine();
+      const bindingsStream = await myEngine.queryBindings(`SELECT ?o WHERE {
+         ?s <http://www.w3.org/ns/pim/space#storage> ?o.
+        }`, {
+        sources: [`${webID}`],
+      });
+      const bindings = await bindingsStream.toArray();
+  
+      const podUrl =bindings[0].get('o').value;
+  
+      const containerUri = podUrl+'public/todosnew/'
+      console.log(containerUri)
+      const file=containerUri.split('Data')[0]+'todos.ttl';
+      console.log(file)
+      setFile(file)
+        
+      const response = await fetch(file, {
+        method: 'GET',
+        headers: {'Content-Type': 'text/turtle'},
+        credentials: 'include',
+      });
+  
+      if (300<response.status&&response.status<500) {
+        console.log('No place to store todos. Hence it will be created');
+        const query = ``;
+  
+        await fetch(file, {
+          method: 'PUT',
+          headers: {'Content-Type': 'text/turtle'},
+          body: query,
+          credentials: 'include',
+        });
+     //Below is used when using inrupt libs   
+    // getTodoListViaQuery(session)
+    //   .then((todos: ITodo[]) => { console.log(todos) })
+    //   .catch((reason: any) => console.log(reason))
+    // void (async () => {
+    //   const profileDataset = await getSolidDataset(session.info.webId as any, {
+    //     fetch: session.fetch as any
+    //   })
+    //   const profileThing = getThing(profileDataset, session.info.webId as any)
+    //   const podsUrls = getUrlAll(profileThing as any, STORAGE_PREDICATE)
+    //   console.log(podsUrls)
+    //   const pod = podsUrls[0]
+    //   const containerUri = `${pod}todos/`
+    //   const list = await getOrCreateTodoList(containerUri, session.fetch)
+    //   setTodos(list)
+    //   // console.log(list)
+    // })()
+  }})();
   }, [session, session.info.isLoggedIn])
 
   function validate(event: React.ChangeEvent<HTMLInputElement>): void {
@@ -93,7 +133,7 @@ const Login: React.FC = (): JSX.Element => {
             />
           </div>
           <section>
-            <InputField todos={todos} setTodos={setTodos} />
+            <InputField todos={todos} setTodos={setTodos} file={file} />
             <TodoList todos={todos} setTodos={setTodos} />
           </section>
         </CombinedDataProvider>
