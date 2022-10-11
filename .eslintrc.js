0061528c716bbcd6c84c651a7492e290d439120a
JSteinbach <jsteinbach@mailbox.org>
2022-10-11 14:53:29 +0200

    Add React hook rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        tsconfigRootDir: __dirname, // this is the reason this is a .js file
-        project: ['./tsconfig.eslint.json'],
-    },
-    extends: [
-        '@rubensworks'
-    ],
-    rules: {
-      'no-implicit-coercion': 'off',
-      'no-control-regex': 'off',
-    }
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname, // this is the reason this is a .js file
+    project: ["./tsconfig.eslint.json"],
+  },
+  extends: ["@rubensworks"],
+  plugins: ["react-hooks"],
+  rules: {
+    "no-implicit-coercion": "off",
+    "no-control-regex": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn", // <--- THIS IS THE NEW RULE
+  },
 };
