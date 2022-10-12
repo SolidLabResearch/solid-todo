aede279bf693cc05d81df46b52be6e368fb44576
JSteinbach <jsteinbach@mailbox.org>
2022-10-12 13:55:42 +0200

    Improve Login

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,13 +7,17 @@ import Login from "./Login";
 import TaskList from "./TaskList";
 import { queryPodUrl } from "../logic/query";
 
+/**
+ *  Login logs into a session
+	The session allows us to get the storage locations on the pod.
+	We need those to display the todo file(s) in the TaskList
+ */
+
 const App = () => {
   const [file, setFile] = useState<string>("");
-  const [podUrl, setPodUrl] = useState<URL>(
-    new URL("https://example.org/podURL")
-  );
   const [webId, setWebId] = useState<URL>(new URL("https://example.org/webID"));
   const { session } = useSession();
+  const [loggedIn, setLoggedIn ] = useState<boolean>(false)
 
   useEffect(() => {
     console.log("before useEffectHook: " + session.info.isLoggedIn);
@@ -29,35 +33,27 @@ const App = () => {
       queryPodUrl(webIdUrl)
         .then((podUrl: URL) => {
           console.log("success: " + podUrl);
-          setPodUrl(podUrl);
+
+          const containerUri: string = podUrl.href + "private/todosnew/";
+          console.log("containerUri is " + containerUri);
+
+          const file: any =
+            (containerUri.split("Data")[0] as string) + ("todos.ttl" as string);
+          console.log("file is " + file);
+          setFile(file);
+			 setLoggedIn(true)
         })
         .catch((reason: any) => console.log(reason));
-
-      /* const containerUri: any =
-  *   (podUrl as string) + ("private/todosnew/" as string);
-  * console.log("Login.tsx :: containerUri is " + containerUri);
-
-  * const file: any =
-  *   (containerUri.split("Data")[0] as string) + ("todos.ttl" as string);
-  * // const file: any = (containerUri.split('Data')[0] as string)
-  * console.log("Login.tsx :: file is " + file);
-  * setFile(file);
-   */
-      /* setWebId(session.info.webId ?? oidcIssuer); */
       //return file; // QUESTION? Why return this? Doesn't useEffect only run cleanup _functions_?
     })();
   }, [session, session.info.isLoggedIn]); // only re-run the effect of session/loggedIn changes
 
-  return session.info.isLoggedIn ? (
+  return loggedIn ? (
     <React.StrictMode>
       <TaskList webId={webId.href} file={file} session={session} />
     </React.StrictMode>
   ) : (
-    <Login
-      session={session}
-      setWebId={setWebId}
-      setFile={setFile}
-    />
+    <Login session={session} setWebId={setWebId} setFile={setFile} />
   );
 };
 
