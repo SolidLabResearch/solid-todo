331e881917367018bf858a3868c121eadfcd9542
VidyashreeTarikere <101635811+VidyashreeTarikereUgent@users.noreply.github.com>
2022-11-21 06:04:05 +0100

    Update InputField.tsx

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -32,7 +32,7 @@ const InputField = ({ todos, setTodos, file, session }: any): any => {
       sodo:dateCreated "${createdDate}".
       }`, context)
       .then(() => { confirm('New task  added to your pod!') })
-      .catch((error) => { alert(`Inserting new task failed: ${error.message}`) })
+      .catch((error) => { alert('Inserting new task failed: ${error.message}') })
     const newTodo: TodoItem = { id, text: todo, status: status === 'true', dateCreated: createdDate }
     setTodos([...todos, newTodo])
 
